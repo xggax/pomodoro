a20556d7435e023298ed1792cc9fc77d85679061
xggax <gabrielgamagga@gmail.com>
2017-11-09 14:17:46 -0400

    ajuste final autenticação com firebase e alerta de cadastrado

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 /**
@@ -20,7 +20,9 @@ export class CadastroPage {
   @ViewChild('nomeusuario') nomeusuario;
   @ViewChild('senha') senha;
 
-  constructor(private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private alertCtrl: AlertController, private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
+    
+
   }
 
   ionViewDidLoad() {
@@ -28,14 +30,24 @@ export class CadastroPage {
   }
 
 
+  alert(mensagem: string){
+    this.alertCtrl.create({
+      title: 'Info!',
+      subTitle: mensagem,
+      buttons: ['OK']
+    }).present();
+  }
+
 
   cadastrarUsuario(){
     this.fire.auth.createUserWithEmailAndPassword(this.nomeusuario.value, this.senha.value)
     .then(data =>{
       console.log('pegou arquivo', data);
+      this.alert('Cadastrado com sucesso!');
     })
     .catch(error => {
       console.log('pegou um erro', error);
+      this.alert(error.message);
     })
 
       console.log('cadastrando: ', this.nomeusuario.value, this.senha.value);
